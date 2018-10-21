cb2cf72f4f51adc0f7f9d537492bd50dbf4eb83a
never2average <priyesh.sriv2017@gmail.com>
2018-10-22 03:09:34 +0530

    PEP 8 Compliance achieved

--- prizeDistribution.py
+++ prizeDistribution.py
@@ -1,132 +1,161 @@
-from math import log10,floor,ceil
-def prettyPrint(List1,List2):
-    rankStart,rankEnd=0,0
-    for i in range(len(List1)):
-        rankStart,rankEnd=rankEnd+1,rankEnd+List1[i]
-        if rankStart==rankEnd:
-            print("{}".format(rankStart),List2[i],sep="    ")
-        else:
-            print("{}-{}".format(rankStart,rankEnd),List2[i],sep="    ")
+from math import log10, floor, ceil
 
-def roundToNice(number):
-    expCount=0
-    while((number>1000 or number%10==0) and number!=0):
-        number//=10
-        expCount+=1
-    if number>=10 and number<100:
-        number=(number//5)*5
-    elif number>=100 and number<250:
-        number=(number//25)*25
-    elif number>=250 and number<1000:
-        number=(number//125)*125
-    number=number*(10**expCount)
+
+# This is just a function to prettify and print out the prize distribution table
+
+def pretty_print(list1, list2):
+    rank_start, rank_end = 0, 0
+    for i in range(len(list1)):
+        rank_start, rank_end = rank_end+1, rank_end+list1[i]
+        if rank_start == rank_end:
+            print("{}".format(rankStart), list2[i], sep="    ")
+        else:
+            print("{}-{}".format(rankStart, rankEnd), list2[i], sep="    ")
+
+# This function will be used to round down numbers to the nearest nice number as defined
+# in the research paper
+
+
+def round_to_nice(number):
+    exp_count = 0
+    while number > 1000 or number % 10 == 0 and number != 0:
+        number = number//10
+        exp_count += 1
+    if 10 <= number < 100:
+        number = (number//5)*5
+    elif 250 > number >= 100:
+        number = (number//25)*25
+    elif 100 > number >= 250:
+        number = (number//125)*125
+    number = number*(10**exp_count)
     return number
 
-def powSum(N,m,entryFee,excess,bucketSizeList):
-    Sum=0
-    index=0
-    count=bucketSizeList[0]
-    bucketedPrizeList=[entryFee*i for i in bucketSizeList]
+# Used to calculate the sum of the power series
 
-    for i in range(1,N+1):
-        value=i**(-1*m)
-        Sum+=value
-        bucketedPrizeList[index]+=excess*value
-        count-=1
-        if count==0:
-            bucketedPrizeList[index]/=bucketSizeList[index]
-            index+=1
+
+def pow_sum(number_winners, m, entry_fee, excess, bucket_size_list):
+    full_sum = 0
+    index = 0
+    count = bucket_size_list[0]
+    bucketed_prize_list = [entry_fee*i for i in bucket_size_list]
+
+    for i in range(1, number_winners+1):
+        value = i**(-1*m)
+        full_sum += value
+        bucketed_prize_list[index] += excess*value
+        count -= 1
+        if count == 0:
+            bucketed_prize_list[index] /= bucket_size_list[index]
+            index += 1
             try:
-                count=bucketSizeList[index]
-            except:
+                count = bucket_size_list[index]
+            except IndexError:
                 break
-    return Sum,bucketedPrizeList
+    return full_sum, bucketed_prize_list
 
-def calculatePrizes(potSize,entryFee,N,P1,bucketSizeList):
-    low,high=0,8
-    excess=P1-entryFee
-    distributionRatio=(potSize-N*entryFee)/excess
-    while(low<=high):
-        mid=(high+low)/2
-        value,List=powSum(N,mid,entryFee,excess,bucketSizeList)
-        if value<0.9999*distributionRatio:
-            high=mid
-        elif value>1.0001*distributionRatio:
-            low=mid
+# Function to calculate all the distinct numbers
+
+
+def calculate_prizes(pot_size, entry_fee, number_winners, p1, bucket_size_list):
+    list_items = []
+    low, high = 0, 8
+    excess = p1-entry_fee
+    distribution_ratio = (pot_size-number_winners*entry_fee)/excess
+    while low <= high:
+        mid = (high+low)/2
+        value, list_items = pow_sum(number_winners, mid, entry_fee, excess, bucket_size_list)
+        if value < 0.9999*distribution_ratio:
+            high = mid
+        elif value > 1.0001*distribution_ratio:
+            low = mid
         else:
             break
-    return List
+    return list_items
+
+# Calculate the number of winners in each bucket
 
 
-def calculateBucketSizes(N,r_max):
-    single_buckets=min(ceil(log10(N))+1,3)
-    bucketSizes=[1 for i in range(single_buckets)]
-    bucketSizes.extend([0 for i in range(r_max-single_buckets)])
-    low,high=1,100
-    r_max-=single_buckets
-    N-=single_buckets
-    if r_max!=0:
-        while(low<=high):
-            mid=(high+low)/2
-            value=mid*(mid**r_max-1)/(mid-1)
-            if value<0.99*N:
-                low=mid
-            elif value>1.01*N:
-                high=mid
+def calculate_bucket_sizes(number_winners, r_max):
+    single_buckets = min(ceil(log10(number_winners))+1, 3)
+    bucket_sizes = [1 for _ in range(single_buckets)]
+    bucket_sizes.extend([0 for _ in range(r_max-single_buckets)])
+    low, high = 1, 100
+    mid = 0
+    r_max -= single_buckets
+    number_winners -= single_buckets
+    if r_max != 0:
+        while low <= high:
+            mid = (high+low)/2
+            value = mid*(mid**r_max-1)/(mid-1)
+            if value < 0.99*number_winners:
+                low = mid
+            elif value > 1.01*number_winners:
+                high = mid
             else:
                 break
-        r_max+=single_buckets
-        N+=single_buckets
-        for i in range(single_buckets,r_max):
-            elementSum=sum(bucketSizes)
-            if ceil(mid*bucketSizes[i-1])+elementSum<=N:
-                if ceil((mid**2)*bucketSizes[i-1])+ceil(mid*bucketSizes[i-1])+elementSum>N:
-                    bucketSizes[i]=floor((N-elementSum)/2)
-                    bucketSizes[i+1]=ceil((N-elementSum)/2)
+        r_max += single_buckets
+        number_winners += single_buckets
+        for i in range(single_buckets, r_max):
+            element_sum = sum(bucket_sizes)
+            if ceil(mid * bucket_sizes[i-1])+element_sum <= number_winners:
+                if ceil((mid**2)*bucket_sizes[i-1])+ceil(mid*bucket_sizes[i-1])+element_sum > number_winners:
+                    bucket_sizes[i] = floor((number_winners-element_sum)/2)
+                    bucket_sizes[i+1] = ceil((number_winners-element_sum)/2)
                     break
-            bucketSizes[i]=ceil(mid*bucketSizes[i-1])
-        while(bucketSizes[-1]==0):
-            bucketSizes.pop(-1)
-        bucketSizes[-1]=0
-        for i in range(len(bucketSizes)-1):
-            bucketSizes[i]=roundToNice(bucketSizes[i])
-        bucketSizes[-1]=N-sum(bucketSizes)
-        return bucketSizes
+            bucket_sizes[i] = ceil(mid*bucket_sizes[i-1])
+        while bucket_sizes[-1] == 0:
+            bucket_sizes.pop(-1)
+        bucket_sizes[-1] = 0
+        for i in range(len(bucket_sizes)-1):
+            bucket_sizes[i] = round_to_nice(bucket_sizes[i])
+        bucket_sizes[-1] = number_winners-sum(bucket_sizes)
+        return bucket_sizes
 
-def nicefy(potSize,bucketSizeList,bucketPrizeList):
-    excess=0
-    for i in range(len(bucketSizeList)):
-        niceNo=roundToNice(int(bucketPrizeList[i]))
-        bucketPrizeList[i]=niceNo
-        excess+=bucketPrizeList[i]*bucketSizeList[i]
-    excess=potSize-excess
-    return excess,bucketPrizeList
+# Makes all the prizes as nice numbers
 
-def allocateExcessFunds(potSize,excess,bucketSizeList,bucketPrizeList):
-    n=len(bucketSizeList)
+
+def nice_numerator(pot_size, bucket_size_list, bucket_prize_list):
+    excess = 0
+    for i in range(len(bucket_size_list)):
+        nice_no = round_to_nice(int(bucket_prize_list[i]))
+        bucket_prize_list[i] = nice_no
+        excess += bucket_prize_list[i]*bucket_size_list[i]
+    excess = pot_size-excess
+    return excess, bucket_prize_list
+
+# Once the ideal amount of prizes have been distributed, some amount will remain
+# So to protect the niceness of the pool size, we will violate some constraints
+
+
+def allocate_excess_funds(excess, bucket_size_list, bucket_prize_list):
+    n = len(bucket_size_list)
     for i in range(n-1):
-        if bucketPrizeList[i]==bucketPrizeList[i+1]:
-            incrementer=((bucketPrizeList[i+1]+bucketPrizeList[i-1])/2-bucketPrizeList[i])*bucketSizeList[i]
-            if incrementer<=excess:
-                bucketPrizeList[i]=(bucketPrizeList[i+1]+bucketPrizeList[i-1])/2
-                excess-=incrementer
-    if excess>=0:
-        index=0
+        if bucket_prize_list[i] == bucket_prize_list[i+1]:
+            incrementer = ((bucket_prize_list[i+1]+bucket_prize_list[i-1])/2-bucket_prize_list[i])*bucket_size_list[i]
+            if incrementer <= excess:
+                bucket_prize_list[i] = (bucket_prize_list[i+1]+bucket_prize_list[i-1])/2
+                excess -= incrementer
+    if excess >= 0:
+        index = 0
         for i in range(n):
-            if bucketSizeList[i]!=1:
-                index=i
+            if bucket_size_list[i] != 1:
+                index = i
                 break
-        bucketPrizeList[index-1]+=excess
-        if bucketPrizeList[index-1]%10==0:
-            bucketPrizeList[index-1]=int(bucketPrizeList[index-1])
-        for i in range(index-1,0,-1):
-            if bucketPrizeList[i]>bucketPrizeList[i-1]:
-                bucketPrizeList[i],bucketPrizeList[i-1]=bucketPrizeList[i-1],bucketPrizeList[i]
-    return bucketPrizeList
+        bucket_prize_list[index-1] += excess
+        if bucket_prize_list[index-1] % 10 == 0:
+            bucket_prize_list[index-1] = int(bucket_prize_list[index-1])
+        for i in range(index-1, 0, -1):
+            if bucket_prize_list[i] > bucket_prize_list[i-1]:
+                bucket_prize_list[i], bucket_prize_list[i-1] = bucket_prize_list[i-1], bucket_prize_list[i]
+    return bucket_prize_list
 
-def heuristicSolution(potSize,entryFee,P1,N,r_max):
-    bucketSizeList=calculateBucketSizes(N,r_max)
-    bucketPrizeList=calculatePrizes(potSize,entryFee,N,P1,bucketSizeList)
-    excess,bucketPrizeList=nicefy(potSize,bucketSizeList,bucketPrizeList)
-    bucketPrizeList=allocateExcessFunds(potSize,excess,bucketSizeList,bucketPrizeList)
-    prettyPrint(bucketSizeList,bucketPrizeList)
+
+# Ties together all components into a neat and clean solution
+
+
+def heuristic_solution(pot_size, entry_fee, p1, number_winners, r_max):
+    bucket_size_list = calculate_bucket_sizes(number_winners, r_max)
+    bucket_prize_list = calculate_prizes(pot_size, entry_fee, number_winners, p1, bucket_size_list)
+    excess, bucket_prize_list = nice_numerator(pot_size, bucket_size_list, bucket_prize_list)
+    bucket_prize_list = allocate_excess_funds(excess, bucket_size_list, bucket_prize_list)
+    pretty_print(bucket_size_list, bucket_prize_list)
